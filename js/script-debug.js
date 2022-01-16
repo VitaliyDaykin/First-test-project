1446ead28daefd77a54bf32d8bc2ff09b877fbf6
Vitaliii Daikin <73659568+VitaliyDaykin@users.noreply.github.com>
2022-01-16 03:13:46 +0200

    First site

---
+++ js/script-debug.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// menu burger
+$(document).ready(function () {
+  $(".menu-burger-btn").on("click", function () {
+    $(".menu-burger-btn, .header__blok-menu").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+}); // accordion
+
+$(document).ready(function () {
+  $(".accordion-spoiler__title").on("click", function () {
+    var parent = $(this).parent();
+
+    if (parent.hasClass("accordion-spoiler__item-active")) {
+      parent.removeClass("accordion-spoiler__item-active");
+    } else {
+      $(".accordion-spoiler__item").removeClass("accordion-spoiler__item-active");
+      parent.addClass("accordion-spoiler__item-active");
+    }
+  });
+}); // carousel
+
+$(".stages-development__carousel-wrapper ").slick({
+  infinite: true,
+  slidesToShow: 1.65,
+  slidesToScroll: 1,
+  autoplay: true,
+  responsive: [{
+    breakpoint: 992,
+    settings: {
+      slidesToShow: 1
+    }
+  }]
+});
+$(".flip-box-mob__carousel-wrapper ").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true
+}); // validation form;
+
+var contactForm = document.getElementById("contactForm");
+contactForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  if (!validation(this)) {
+    this.submit();
+  }
+});
+
+function validation(contactForm) {
+  var isError = false;
+
+  for (var i = 0; i < contactForm.length; i++) {
+    var elementForm = contactForm[i];
+
+    if (elementForm.dataset.required && elementForm.value === "") {
+      isError = true;
+      elementForm.classList.add("error");
+      elementForm.addEventListener("input", remuveErrorClass);
+    }
+  }
+
+  return isError;
+}
+
+function remuveErrorClass() {
+  this.classList.remove("error");
+  this.removeEventListener("input", remuveErrorClass);
+}
